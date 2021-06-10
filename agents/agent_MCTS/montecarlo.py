2141cdf9127ac5a4aa9df28e30d235be9e0cba15
Haseeb <haseeb.javed@gmail.com>
2021-06-10 14:06:38 +0200

    adding the monte carlo tree search

--- agents/agent_MCTS/montecarlo.py
+++ agents/agent_MCTS/montecarlo.py
@@ -2,28 +2,62 @@ import numpy as np
 from agents.common import PlayerAction, apply_player_action, BoardPiece, available_columns, opponent, connected_four, check_end_state, PLAYER2, PLAYER1, NO_PLAYER, SavedState, GenMove, GameState
 from typing import Optional, Callable, Tuple
 
-def mcts(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState] = None
-) -> Tuple[int, Optional[SavedState]]:
+class Node:
+    def __init__(self, board): # constructor
+        self.wins = None # or 0
+        self.simulations = None # or 0
+        self.parent = None
+        self.children = [] # or a single child as in self.child = None ?
+        self.board = board
+
+    def value(self):
+        return self.wins / self.parent.wins + (c * np.sqrt(np.log(self.parent.simulations) / self.simulations)
+
+class MCTS:   # how do I pass the board into MCTS?
+    def __init__(self):  # constructor
+        self.root = None  # empty tree
+        self.height = -1
+
+    def expand(self, parent):  # insert node
+        """
+        Create one child node at a time, run a simulation, and back propagate the results.
+        Since simulations of each child node are at random, we don't save those grandchildren nodes as they were
+        created randomly and used randomly.
+        We only save nodes in the MCTS that have some logic such as UCB1.
+        :param: parent node
+        :return:
+        """
+        if self.root is None:
+            self.root = Node()
+            self.height += 1
+        else:
+            avail_cols = available_columns(board)  # how do I pass the board into MCTS?
+
+        find available_columns, create a child in a column as long as columns are available
+
+    def simulate(self):
+        """
+        for each newly created child node, run a simulation of random moves all the way down to a win/draw/loss.
+        save results and back propagate the results all the way to root.
+        :return:
+        """
+
+
+    def select(self, Node, c):
+        """
+        Don't select until all children have been create for the parent with one simulation each
+
+        """
+        chosen_node = Node.children.index(max(Node.children.value)) # will .value work on a list or should I iterate?
+
+        return chosen_node
+
+
+#def mcts(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState] = None
+#) -> Tuple[int, Optional[SavedState]]:
     """
     Build a Monte Carlo Tree Search algorithm that takes a board at any state (beginning or middle of the game
     and determines the most valuable move for a player.
-    The MCTS algorithm at any board state builds out the search tree iteratively and determines the most valuable move
-    which will give the most wins.
-    Start at the root node which is the given state of the board, and build out different branches corresponding to
-    available moves (columns).
-    Within each branch, make random moves until you reach the end, i.e. a leaf node i.e. no
-    more moves or indices available to make a legitimate move.
-    Then determine what the board state is at the end, i.e. a Win, Loss, or a Draw. Score each end state and associate
-    this value to the branch, store it, and also provide it to the root.
-    The root can do the same for each available branch (column).
-    After one iteration of each branch, the root has an idea of the value of each branch (for Exploitation), but based
-    on random moves within each branch. We would get different outcomes if the random values were different.
-    Therefore run more iterations on each branch (Exploration) to find new outcomes and update each branch score
-    for instance after one round and one win (1/1), and after three rounds and two wins (2/3), etc. and store in
-    root node as well.
-    Determine how many iterations are needed before the statistics for each branch are more reliable than just one
-    iteration.
-    Then choose the highest value branch and provide the column number (action) to the player.
 
     Keyword arguments:
         board: the board that the player is playing and trying to win
@@ -33,3 +67,5 @@ def mcts(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState
     Returns:
         Tuple: consisting of the location of the column of the best move, and the Optional Saved State
     """
+
+
